e3d7745c4fc9fd740666678e6206a45be50aa309
Brandon <brandon.ebarnett@gmail.com>
2022-08-27 09:45:27 -0400

    Add project files.

---
+++ Exercise002/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Write a program that takes two numbers from the user and displays the maximum of the two. */
+
+Console.WriteLine("--------------------------------------------------------------------");
+Console.WriteLine("-----------------------Display Maximum Number-----------------------");
+Console.WriteLine("--------------------------------------------------------------------");
+Console.WriteLine();
+Console.Write("Please enter your first number: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Please enter your second number: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The maximum number you entered was: " + Math.Max(numberOne, numberTwo));
